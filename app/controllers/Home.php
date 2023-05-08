5138faa2c82f2a7eeda5bc43150ae3fd4becf85d
Torion12 <adrian.torion12@gmail.com>
2023-05-08 16:28:21 -0500

    May 09, 2023 My first Commit

---
+++ app/controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+
+class Home {
+    public function index(){
+        echo"view home page ";
+    }
+
+    public function edit(){
+        echo"view edit ";
+    }
+
+    public function delete(){
+        echo"view delete ";
+    }
+
+    
+
+    
+
+}
---
+++ app/controllers/_404.php
@@ -0,0 +1,7 @@
+<?php
+
+class _404{
+        function index(){
+            print_r("PAGE NOT FOUND");
+        }
+}
